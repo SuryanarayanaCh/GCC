023326e3a7b47580dc2946da86a6b26c6905b4d1
Santhosh3009 <santhoshpamula3009@gmail.com>
2022-08-11 12:22:20 +0530

    final

--- src/Components/ClassCard.js
+++ src/Components/ClassCard.js
@@ -1,10 +1,33 @@
 import { IconButton } from "@material-ui/core";
 import { AssignmentIndOutlined, FolderOpenOutlined } from "@material-ui/icons";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import "./ClassCard.css";
-function ClassCard({ name, creatorName, coursecode, creatorPhoto, id, style,creatorMail }) {
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../firebase";
+  
+function ClassCard({ name, creatorName, coursecode, creatorPhoto, id, style }) {
+  const [user, loading, error] = useAuthState(auth);
+  const [classId, setClassId] = useState(id);
+  
+  
+  const [classData,setClassData] = useState([]);
+
+  useEffect(() =>{
+    const getDocs = async () => {
+      const classRef = await db.collection("classes").doc(classId).get();
+      const tempData = classRef.data();
+      console.log(tempData);
+      
+  
+     setClassData(tempData);
+    
+    }
+    if(classId!==""){getDocs();}
+  },[classId]);
+ 
+ 
+
   const history = useNavigate();
   const goToClass = () => {
     history(`/class/${id}`);
@@ -12,13 +35,20 @@ function ClassCard({ name, creatorName, coursecode, creatorPhoto, id, style,crea
   return (
     <div className="classCard" style={style} onClick={goToClass}>
       <div className="classCard__upper">
-        <div className="classCard__className">{name}</div>
+        <div className="classCard__className">
+          {name}
+          {classData.creatorUid === user.uid && 
+          <img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Gold_Star_%28with_border%29.svg" className="star"/>
+          }
+        </div>
         <div className="classCard__coursecode">{coursecode}</div>
-        <div className="classCard__creatorName">{creatorName}</div>
+       {classData.creatorUid !== user.uid && <div className="classCard__creatorName">{creatorName}</div>}
+        
         <img src={creatorPhoto} className="classCard__creatorPhoto" />
       </div>
       <div className="classCard__middle"></div>
       <div className="classCard__lower">
+      
         <IconButton>
           <FolderOpenOutlined />
         </IconButton>
